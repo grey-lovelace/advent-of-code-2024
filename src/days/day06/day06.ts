9a136fa29980ae9d57e287f9255e423d94369823
Grey Lovelace <austin.grey.lovelace@gmail.com>
2024-12-06 02:50:38 -0600

    Cleanup day06

--- src/days/day06/day06.ts
+++ src/days/day06/day06.ts
@@ -1,5 +1,5 @@
 import { Direction, turnR } from "../../utils/directions.ts";
-import { Point, stringToGrid } from "../../utils/grid.ts";
+import { stringToGrid } from "../../utils/grid.ts";
 import Day from "../day.ts";
 
 export default class Day06 extends Day {
@@ -7,73 +7,44 @@ export default class Day06 extends Day {
   expectedPart1Results = () => [["sample.txt", 41]];
   expectedPart2Results = () => [["sample.txt", 6]];
 
-  part1 = (input: string) => {
-    const startingPoint = input
-      .let(stringToGrid)
-      .points.find((point) => point.val === "^");
-    let currentPoint = startingPoint!;
-    let vistedPoints = new Set([`${currentPoint.x},${currentPoint.y}`]);
-    let currentDir: Direction = "north";
-    while (currentPoint[currentDir]() !== undefined) {
-      if (currentPoint[currentDir]().val === "#") {
-        currentDir = turnR(currentDir);
-      } else {
-        currentPoint = currentPoint[currentDir]();
-        vistedPoints.add(`${currentPoint.x},${currentPoint.y}`);
-      }
-    }
-    return vistedPoints.size;
-  };
-
-  part2 = (input: string) => {
-    const startingPoint = input
-      .let(stringToGrid)
-      .points.find((point) => point.val === "^");
-    let currentPoint = startingPoint!;
-    let vistedPoints = new Set([`${currentPoint.x},${currentPoint.y}`]);
-    let currentDir: Direction = "north";
-    while (currentPoint[currentDir]() !== undefined) {
-      if (currentPoint[currentDir]().val === "#") {
-        currentDir = turnR(currentDir);
-      } else {
-        currentPoint = currentPoint[currentDir]();
-        vistedPoints.add(`${currentPoint.x},${currentPoint.y}`);
-      }
-    }
-    return [...vistedPoints].slice(1).count((testPoint) => {
-      currentPoint = startingPoint!;
-      let newVistedPoints = new Set([`${currentPoint.x},${currentPoint.y}`]);
-      currentDir = "north";
-      let vistedPointsWithDir = [
-        `${currentPoint.x},${currentPoint.y},${currentDir}`,
-      ];
-      while (currentPoint[currentDir]() !== undefined) {
-        if (
-          currentPoint[currentDir]().val === "#" ||
-          `${currentPoint[currentDir]().x},${currentPoint[currentDir]().y}` ===
-            testPoint
-        ) {
-          currentDir = turnR(currentDir);
-        } else if (
-          vistedPointsWithDir.includes(
-            `${currentPoint[currentDir]().x},${
-              currentPoint[currentDir]().y
-            },${currentDir}`
-          )
-        ) {
-          return true;
-        } else {
-          currentPoint = currentPoint[currentDir]();
-          newVistedPoints.add(`${currentPoint.x},${currentPoint.y}`);
-          vistedPointsWithDir.push(
-            `${currentPoint.x},${currentPoint.y},${currentDir}`
-          );
-        }
-      }
-      return false;
-    });
-  };
+  part1 = (input: string) => input.let(findPath).visitedPoints.length;
+  part2 = (input: string) =>
+    input
+      .let(findPath)
+      .visitedPoints.slice(1)
+      .count((point) => findPath(input, point).loopFound);
 }
+
+const findPath = (input: string, testObstacle: string = `-1,-1`) => {
+  const [testX, testY] = testObstacle.split(",").toNums();
+  let currentDir: Direction = "north";
+  let currentPoint = input
+    .let(stringToGrid)
+    .also((grid) => grid.at(testX, testY) && (grid.at(testX, testY)!.val = "#"))
+    .points.find((point) => point.val === "^")!;
+  let loopFound = false;
+  const visited = new Set([[currentPoint.x, currentPoint.y, currentDir].join(",")]);
+  while (currentPoint[currentDir]() !== undefined) {
+    const nextPoint = currentPoint[currentDir]()!;
+    if (
+      visited.has([nextPoint.x, nextPoint.y, currentDir].join(","))
+    ) {
+      loopFound = true;
+      break;
+    } else if (nextPoint.val === "#") {
+      currentDir = turnR(currentDir);
+    } else {
+      currentPoint = nextPoint;
+      visited.add([currentPoint.x, currentPoint.y, currentDir].join(","));
+    }
+  }
+  return {
+    visitedPoints: [...visited]
+      .map((point) => point.split(",").toSpliced(2, 1).join(","))
+      .unique(),
+    loopFound,
+  };
+};
 
 if (import.meta.main) {
   new Day06().run();
